ea7b21bf09435d24c12b0cc9072ad4b9552d04b0
ayushbabar3313-create <ayushbabar3313@gmail.com>
2026-04-15 12:29:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const apiKey = "YOUR_API_KEY";
+const apiKey = "ecb8e736e52d464095bb14771c8fe103";
 
 // Mumbai AQI
-fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=19.0760&lon=72.8777&appid=${apiKey}`)
+fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=19.0760&lon=72.8777&appid=${ecb8e736e52d464095bb14771c8fe103}`)
 .then(res => res.json())
 .then(data => {
   document.getElementById("mumbai-aqi").innerText = data.list[0].main.aqi;
 });
 
 // Oslo AQI
-fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=59.9139&lon=10.7522&appid=${apiKey}`)
+fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=59.9139&lon=10.7522&appid=${1364fbe9bb23c6d92e3948651874f60c}`)
 .then(res => res.json())
 .then(data => {
   document.getElementById("oslo-aqi").innerText = data.list[0].main.aqi;
